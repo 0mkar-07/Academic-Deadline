755ec15fdb3d9802f815d3956d9b026175ef4a5c
0mkar-07 <omkarksheerasagar07@gmail.com>
2026-03-19 18:23:46 +0000

    checking Task.cpp and Task.h

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@ using namespace std;
 #include "Task.h"
 
 int main(){
+    int id;
+    string s;
 
-    Task one;
-    
+    cout<<"enter ID : ";
+    cin>>id;
+    cin.ignore(); // ignore \n created by above cin. so getline can take input
+    cout<<"enter title : ";
+    getline(cin,s);
+
+    Task t1(id,s);
+    t1.display();
+    t1.isCompleted();
+    t1.display();
+
+
+
 
 }
