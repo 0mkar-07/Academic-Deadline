d1fac50cfe2859f2420715cec5c2db52c11ec0bb
0mkar-07 <omkarksheerasagar07@gmail.com>
2026-03-19 19:07:04 +0000

    add assignment.cpp and assignment.h

---
+++ assignment.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "assignment.h"
+using namespace std;
+
+void Assignment :: display(){
+    cout<<"subject : "<<subject<<endl;
+    cout<<"deadline : "<<deadline<<endl;
+}
---
+++ assignment.h
@@ -0,0 +1,19 @@
+#ifndef ASSIGNMENT_H
+#define ASSIGNMENT_H
+#include "Task.h"
+#include<string>
+
+class Assignment : public Task{
+
+    private:
+        std::string subject;
+        std::string deadline; 
+
+    public:
+        Assignment(int id,std::string detail,std :: string sub,std :: string date ) : Task(id,detail),subject(sub),deadline(date){};
+        void display();//override
+
+
+};
+
+#endif
