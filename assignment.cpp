14fd46bd1aeeb16c83abc0116ccd6c049912affa
0mkar-07 <omkarksheerasagar07@gmail.com>
2026-03-20 12:35:22 +0000

    updated display function in assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 void Assignment :: display(){
+    cout<<endl<<"ID : "<<unique_id<<endl;
+    cout<<"Title : "<<title<<endl;
+    
+    if(completed)
+    cout<<"Status : completed "<<endl;
+    else
+    cout<<"Status : not completed"<<endl;
+
     cout<<"subject : "<<subject<<endl;
     cout<<"deadline : "<<deadline<<endl;
 }
