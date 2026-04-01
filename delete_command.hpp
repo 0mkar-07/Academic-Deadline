6fb3aca15905a82b111a59b9de68b3f98bc15b31
0mkar-07 <omkarksheerasagar07@gmail.com>
2026-04-01 11:26:36 +0000

    add delete command

---
+++ delete_command.hpp
@@ -0,0 +1,30 @@
+#include "CLI/CLI.hpp"
+#include "TaskManager.h"
+#include "icommand.hpp"
+
+class DeleteCommand:ICommand{
+
+private:
+    TaskManager *tm;
+    int id;
+
+    void execute(){
+
+        tm->deleteAssignment(id);
+
+    }
+public:
+    DeleteCommand(TaskManager &tm_ref){
+        tm=&tm_ref;
+    }
+
+    void setup(CLI::App &app){
+
+        auto deletex=app.add_subcommand("delete","Delete assignment of <id>");
+
+        deletex->add_option("delete",id,"Enter id");
+
+        deletex->callback([this](){execute();});
+    }
+};
+
